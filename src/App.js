8dcd4d6b910c6ad0895ff1734cdcfac4472cf8b4
ssuarezs <suarez.santiago.113@gmail.com>
2021-10-21 12:36:54 -0500

    doing Gallery Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react'
-import Header from './components/header'
-import Images from './components/images'
+import Gallery from './learned/projects/gallery/Gallery'
 
-const useImages = (initial) => {
-  const [images, setImages] = useState(initial)
-  const handleImages = async (melo) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const data = await response.json()
-    setImages(data)
-  }
-  return [images, handleImages]
-}
 const App = () => { 
-  const [images, handleImages] = useImages([])
   return(
     <div>
-      <Header handleImages={handleImages}/>
-      <Images images={images}/>
+      <Gallery/>
     </div>
   )
 }
-export default App;
 
+export default App;
---
+++ src/learned/projects/gallery/Gallery.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import './article.css'
+
+const Gallery = () => { 
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({ photos })
+  return(
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID _1HEn33XbuhA-8sX8NRde_JCPnX9iJmrLAHM6sIYlcY'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
+        >
+          <Form>
+            <Field name="search"/>
+          </Form>
+        </Formik>
+      </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+          <article key={photo.id} onClick={() => open(photo.links.html)}>
+            <img src={photo.urls.regular}/>
+            <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+          </article>)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Gallery;
